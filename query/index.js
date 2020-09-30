dd72fc590baccff4aae321abc603490c0e4dc20b
Lachlan McWilliam <mcwilliam01@gmail.com>
2020-09-30 00:34:12 -0700

    implimented query service

--- query/index.js
+++ query/index.js
@@ -9,9 +9,28 @@ app.use(cors());
 
 const posts = {};
 
-app.post("/posts", (req, res) => {});
+app.post("/posts", (req, res) => {
+  res.send(post);
+});
 
-app.post("/events", (req, res) => {});
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  res.send({ status: "ok" });
+});
 
 app.listen(4002, () => {
   console.log("query listening on port: 4002");
